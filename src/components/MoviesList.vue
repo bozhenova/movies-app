68e73067a24dc955bcc024e25430c0054b71194e
Margo <margo.bozhenova@gmail.com>
2020-08-27 17:19:23 +0300

    Add MoviesList component

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,42 @@
+<template>
+  <BContainer>
+  <h3 class="list-title">IMDP Top 250</h3>
+  <BRow>
+  <template v-if="doesExist">
+  <BCol cols="3" v-for="(movie, key) in list" :key="key">
+    <MovieItem :movie="movie"/>
+  </BCol>
+  </template>
+  <template v-else><div>Empty List</div></template>
+  </BRow>
+  </BContainer>
+</template>
+
+<script>
+import MovieItem from './MovieItem'
+
+export default {
+  name: 'MoviesList',
+  props: {
+  list: {
+    type: Object,
+    default: () => {}
+
+  }
+  },
+  components: {
+  MovieItem
+  },
+  computed: {
+  doesExist(){
+  return Boolean(Object.keys(this.list).length)
+  }
+  }
+}
+</script>
+<style scoped>
+.list-title {
+  font-size: 50px;
+  margin-bottom: 30px;
+}
+</style>
