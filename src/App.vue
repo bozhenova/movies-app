9daab4bb812740badeb21add086d1a4667ef14e8
Margo <margo.bozhenova@gmail.com>
2020-08-28 01:11:56 +0300

    Add Router

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,67 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination
+      :current-page="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
+import MoviesList from '@/components/MoviesList';
+import PosterBg from '@/components/PosterBg';
+import MoviesPagination from '@/components/MoviesPagination';
 
 export default {
   name: 'App',
-  components: {},
-  mounted() {
-    this.fetchMovies();
+  components: {
+    MoviesList,
+    PosterBg,
+    MoviesPagination
+  },
+  data: () => ({
+    posterBg: ''
+  }),
+  computed: {
+    ...mapGetters('movies', [
+      'moviesList',
+      'currentPage',
+      'moviesPerPage',
+      'moviesLength'
+    ])
+  },
+  watch: {
+    '$route.query': {
+      handler: 'onPageQueryChange',
+      immediate: true,
+      deep: true
+    }
   },
   methods: {
-    ...mapActions('movies', ['fetchMovies'])
+    ...mapActions('movies', ['changeCurrentPage']),
+    onPageQueryChange({ page = 1 }) {
+      this.changeCurrentPage(Number(page));
+    },
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.$router.push({ query: { page } });
+    }
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  position: relative;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
